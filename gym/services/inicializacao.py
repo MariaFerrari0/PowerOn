887f657e773c43bc711e89b90d69b7ca710866aa
MariaFerrari0 <marialuisaferraridemorais@gmail.com>
2026-09-22 21:51:35 -0300

    test

---
+++ gym/services/inicializacao.py
@@ -0,0 +1,74 @@
+from ..persistencia.arquivo_alunos import ArquivoAlunos
+from ..persistencia.arquivo_professores import ArquivoProfessores
+from ..persistencia.arquivo_modalidades import ArquivoModalidades
+from ..persistencia.arquivo_matriculas import ArquivoMatriculas
+
+from ..indices.indice_alunos import IndiceAlunos
+from ..indices.indice_professores import IndiceProfessores
+from ..indices.indice_modalidades import IndiceModalidades
+from ..indices.indice_matriculas import IndiceMatriculas
+
+from .aluno_service import AlunoService
+from .professor_service import ProfessorService
+from .modalidade_service import ModalidadeService
+from .matricula_service import MatriculaService
+from .faturamento_service import FaturamentoService
+
+
+# =========================
+# ARQUIVOS
+# =========================
+
+arquivo_alunos = ArquivoAlunos()
+arquivo_professores = ArquivoProfessores()
+arquivo_modalidades = ArquivoModalidades()
+arquivo_matriculas = ArquivoMatriculas()
+
+
+# =========================
+# ÍNDICES
+# =========================
+
+indice_alunos = IndiceAlunos(arquivo_alunos)
+indice_professores = IndiceProfessores(arquivo_professores)
+indice_modalidades = IndiceModalidades(arquivo_modalidades)
+indice_matriculas = IndiceMatriculas(arquivo_matriculas)
+
+
+# =========================
+# SERVICES
+# =========================
+
+aluno_service = AlunoService(
+    arquivo_alunos,
+    indice_alunos
+)
+
+professor_service = ProfessorService(
+    arquivo_professores,
+    indice_professores
+)
+
+modalidade_service = ModalidadeService(
+    arquivo_modalidades,
+    indice_modalidades,
+    indice_professores
+)
+
+matricula_service = MatriculaService(
+    arquivo_matriculas,
+    indice_matriculas,
+    arquivo_alunos,
+    indice_alunos,
+    arquivo_modalidades,
+    indice_modalidades
+)
+
+faturamento_service = FaturamentoService(
+    arquivo_modalidades,
+    indice_modalidades,
+    arquivo_matriculas,
+    indice_matriculas,
+    arquivo_professores,
+    indice_professores
+)
--- gym/views/home.py
+++ gym/views/home.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+from ..services.inicializacao import (
+    aluno_service,
+    professor_service,
+    modalidade_service,
+    matricula_service,
+)
+
+
+def index(request):
+    alunos = aluno_service.listar()
+    professores = professor_service.listar()
+    modalidades = modalidade_service.listar()
+    matriculas = matricula_service.listar()
+
+    contexto = {
+        "total_alunos": len(alunos),
+        "total_professores": len(professores),
+        "total_modalidades": len(modalidades),
+        "total_matriculas": len(matriculas),
+    }
+
+    return render(
+        request,
+        "gym/home/index.html",
+        contexto
+    )
